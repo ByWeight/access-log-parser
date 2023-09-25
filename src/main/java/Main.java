daa9b05caa62d044089910c5ff7412582cfb0f20
ByWeight <stdrum@mail.ru>
2023-09-25 14:55:48 +0300

    Create math operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,10 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Введите текст и нажмите <Enter>: " + Math.random());
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
-
+        System.out.println("Введите первое число: ");
+        int firstNumber = new Scanner(System.in).nextInt();
+        //int firstNumber = 5;
+        System.out.println("Введите второе число: ");
+        int secondNumber = new Scanner(System.in).nextInt();
+        //int secondNumber = 5;
+        int sum = firstNumber + secondNumber;
+        int raznost = firstNumber - secondNumber;
+        int proizvedenie = firstNumber * secondNumber;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма: " + sum + "\n" + "Разность: " + raznost + "\n" + "Произведение: " + proizvedenie + "\n" + "Частное: " + quotient);
     }
 }
