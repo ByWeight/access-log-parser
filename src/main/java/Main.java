fbba17a01083365e4f3c03a43bc3ea78bf854da9
ByWeight <stdrum@mail.ru>
2023-11-20 14:01:04 +0300

    Create fileExists and Directory

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,29 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Введите первое число: ");
-        int firstNumber = new Scanner(System.in).nextInt();
-        //int firstNumber = 5;
-        System.out.println("Введите второе число: ");
-        int secondNumber = new Scanner(System.in).nextInt();
-        //int secondNumber = 5;
-        int sum = firstNumber + secondNumber;
-        int raznost = firstNumber - secondNumber;
-        int proizvedenie = firstNumber * secondNumber;
-        double quotient = (double) firstNumber / secondNumber;
-        System.out.println("Сумма: " + sum + "\n" + "Разность: " + raznost + "\n" + "Произведение: " + proizvedenie + "\n" + "Частное: " + quotient);
+
+    public static void main(String[] args){
+
+        int count = 0;
+        while (true) {
+            System.out.println("Введите путь к файлу: ");
+            String path = new Scanner(System.in).nextLine(); // ввод пути к файлу
+            File file = new File(path);
+            if (path.equals("0")){ // выход из цикла
+                break;
+            }
+            boolean fileExists = file.exists(); // указан ли путь к файлу (true если да, false, если нет) isFile(проверяет именно файл)
+            boolean isDirectory = file.isDirectory(); //является ли путь к папке
+            if (fileExists && !isDirectory) {
+                count++;
+                System.out.println("Путь к файлу указан верно ");
+                System.out.println("Это файл "+path+" № " + count);
+            } else {
+                System.out.println("Путь указан неверно или файл не существует " + path);
+            }
+        }
     }
 }
